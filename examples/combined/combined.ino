20d2089fb66811d68fa50814df22ad9378ecdd7a
maxwell <mullinmax@gmail.com>
2017-09-26 00:31:10 -0400

    SD card mounting. not yet parsing quite right

--- examples/combined/combined.ino
+++ examples/combined/combined.ino
@@ -15,20 +15,17 @@ void setup() {
   delay(20000);
   SerialUSB.begin(9600);
   // while (!SerialUSB);
-  if (!SD.begin(chipSelect)) {
-    SerialUSB.println("SD card failed, or not present");
-  }
   SerialUSB.println("Please swipe your RFID Tag.");
 
   if (!rtc.begin()) {
     SerialUSB.println("Couldn't find RTC");
   }
   // following line sets the RTC to the date & time this sketch was compiled
-  //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   load_settings();
-  dataFile = SD.open("data.csv", FILE_WRITE);
-  dataFile.print(':\n');
-  dataFile.close();
+  /*dataFile = SD.open("data.csv", FILE_WRITE);
+    dataFile.print(':\n');
+    dataFile.close();*/
 }
 
 void loop() {
@@ -51,21 +48,21 @@ void loop() {
     SerialUSB.println("No Tag Detected");
   }
 
-    DateTime now = rtc.now();
-    SerialUSB.print(now.year(), DEC);
-    SerialUSB.print('/');
-    SerialUSB.print(now.month(), DEC);
-    SerialUSB.print('/');
-    SerialUSB.print(now.day(), DEC);
-    SerialUSB.print(" (");
-    SerialUSB.print(daysOfTheWeek[now.dayOfTheWeek()]);
-    SerialUSB.print(") ");
-    SerialUSB.print(now.hour(), DEC);
-    SerialUSB.print(':');
-    SerialUSB.print(now.minute(), DEC);
-    SerialUSB.print(':');
-    SerialUSB.print(now.second(), DEC);
-    SerialUSB.println();
+  DateTime now = rtc.now();
+  SerialUSB.print(now.year(), DEC);
+  SerialUSB.print('/');
+  SerialUSB.print(now.month(), DEC);
+  SerialUSB.print('/');
+  SerialUSB.print(now.day(), DEC);
+  SerialUSB.print(" (");
+  SerialUSB.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  SerialUSB.print(") ");
+  SerialUSB.print(now.hour(), DEC);
+  SerialUSB.print(':');
+  SerialUSB.print(now.minute(), DEC);
+  SerialUSB.print(':');
+  SerialUSB.print(now.second(), DEC);
+  SerialUSB.println();
   delay(3000);
 }
 
@@ -79,58 +76,54 @@ void load_settings() {
     return;
   }
   File settings = SD.open("settings.txt");
-  if (settings) {
-    char letter;
-    while (settings.available()) {
-      letter = settings.read();
-      switch (letter) {
-        case '#'://after a comment symbol ignore until new line
-          while (settings.available() && letter != '\n' && letter != '\r') {
-            letter = settings.read();
-          }
-          break;
-        case '\t':
-        case '\n':
-        case '\r':
-        case ' ': //do nothing with spaces new lines and tabs
-          break;
-        case ':':
-          while (settings.available() && value[value_index] != ';') { //read in setting value
-            value[value_index] = settings.read();
-            value_index++;
-          }
-
-          if (current_setting == "DEFAULT_MODE") { //figure out what the setting is
-            SerialUSB.println("boot mode setting parsed");
-          } else if (current_setting == "POWER_SCHEDULE") {
-            SerialUSB.println("power schedule setting parsed");
-          } else {
-            SerialUSB.println(current_setting);
-          }
-
-          while (value_index > 0) {  //clear value for next use
-            value[value_index] = '\0';
-            value_index--;
-          }
-
-          while (next_index > 0) {  //clear setting for next use
-            current_setting[next_index] = '\0';
-            next_index--;
-          }
-          next_index = 0; //reset index
-          break;
-        default:
-          current_setting[next_index] = letter;
-          next_index++;
-          break;
-      }
-    }
-    settings.close();
-  } else {
+  if (!settings) {
     // if the file didn't open, print an error:
-    SerialUSB.println("error opening test.txt");
+    SerialUSB.println("Error, SD card detected but could not open settings.txt");
   }
+  char letter;
+  while (settings.available()) {
+    letter = settings.read();
+    switch (letter) {
+      case '#'://after a comment symbol ignore until new line
+        while (settings.available() && letter != '\n' && letter != '\r') {
+          letter = settings.read();
+        }
+        break;
+      case '\t':
+      case '\n':
+      case '\r':
+      case ' ': //do nothing with spaces new lines and tabs
+        break;
+      case ':':
+        while (settings.available() && value[value_index] != ';') { //read in setting value
+          value[value_index] = settings.read();
+          value_index++;
+        }
 
+        if (current_setting == "DEFAULT_MODE") { //figure out what the setting is
+          SerialUSB.println("boot mode setting parsed");
+        } else if (current_setting == "POWER_SCHEDULE") {
+          SerialUSB.println("power schedule setting parsed");
+        } else {
+          SerialUSB.println(current_setting);
+        }
 
+        while (value_index > 0) {  //clear value for next use
+          value[value_index] = '\0';
+          value_index--;
+        }
+
+        while (next_index > 0) {  //clear setting for next use
+          current_setting[next_index] = '\0';
+          next_index--;
+        }
+        next_index = 0; //reset index
+        break;
+      default:
+        current_setting[next_index] = letter;
+        next_index++;
+        break;
+    }
+  }
+  settings.close();
 }
-//test
